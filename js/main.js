33a626bb7dd11eddf090d405538a17db123b6d8f
jeffersonss-tech <jeffersonssantos92@gmail.com>
2022-01-20 22:06:51 -0300

    refref

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
-var titulo = document.querySelector('.titulo')
-titulo.textContent = "Aparecida Nutricionista"
+var titulo = document.querySelector("#titulo");
+titulo.textContent = "aparecida nutricionista";
 
-var paciente = document.querySelector('#primeiro_paciente')
+var pacientes = document.querySelectorAll(".paciente")
+for (var e = 0; e < pacientes.length; e++){
+    console.log(pacientes)
+    var paciente= pacientes[e]
 
-var tdaltura = paciente.querySelector('.info-altura')
-var tdpeso = paciente.querySelector('.info-peso')
-var tdnome = paciente.querySelector('.info-nome')
-tdnome = tdnome.textContent = 'jeferson'
-tdaltura = tdaltura.textContent = '1.65'
-tdpeso = tdpeso.textContent = '60'
-var imc = paciente.querySelector('.info-imc')
-var  calculo = tdpeso / (tdaltura * tdaltura)
+    var nome = paciente.querySelector(".info-nome")
+    nome = nome.textContent
+    var peso = paciente.querySelector(".info-peso")
+    peso = peso.textContent
+    var altura = paciente.querySelector(".info-altura")
+    altura = altura.textContent
+    var imc = paciente.querySelector(".info-imc")
 
-if (tdpeso <= 0 || tdaltura <= 0 || tdpeso > 400 || tdaltura > 2.00){
-    imc.textContent = 'peso e/ou altura inválido(s)'
-}
-else {
-    imc.textContent = calculo.toFixed(1)
-    console.log('o imc de', tdnome, 'é',calculo)
+    var imc_calc = peso / (altura * altura)
+
+
+    if (peso <= 0 || peso >= 1000 ){
+        imc.textContent = 'peso invalido'
+        paciente.classList.add('paciente-ivalido')
+        paciente.style.color = 'black'
+
+    }
+    else if (altura > 2.5 || altura <= 0){
+        imc.textContent = 'altura invalida'
+        paciente.classList.add('paciente-ivalido')
+        paciente.style.color = 'black'
+    }
+    else 
+        imc.textContent = imc_calc.toFixed(1)
+        paciente.style.background = ''
+        imc.style.background = 'green'
+        imc.style.color = 'white'
 }
