8353051ee2e9d730e4b2ccedf655dd43aaae3857
jeffersonss-tech <jeffersonssantos92@gmail.com>
2022-01-20 23:40:48 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ titulo.textContent = "aparecida nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente")
 for (var e = 0; e < pacientes.length; e++){
-    console.log(pacientes)
     var paciente= pacientes[e]
 
     var nome = paciente.querySelector(".info-nome")
@@ -34,3 +33,41 @@ for (var e = 0; e < pacientes.length; e++){
         imc.style.background = 'green'
         imc.style.color = 'white'
 }
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente')
+botaoAdicionar.addEventListener('click', function(event){
+    event.preventDefault()
+    
+    var form = document.querySelector('#form-adiciona')
+    var nome = form.nome.value
+    var peso = form.peso.value
+    var altura = form.altura.value
+    var gordura = form.gordura.value
+
+    var pacienteTr = document.createElement('tr')
+    
+    var nomeTd = document.createElement('td')
+    var pesoTd = document.createElement('td')
+    var alturaTd = document.createElement('td')
+    var gorduraTd = document.createElement('td')
+    var imcTd = document.createElement('td')
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+    pacienteTr.appendChild(imcTd)
+
+    var tabela = document.querySelector('#tabela-pacientes')
+
+    tabela.appendChild(pacienteTr)
+
+    console.log(pacienteTr)
+
+})
+console.log(botaoAdicionar)
